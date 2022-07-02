550cb653250babcb1a915e537c3bce2b10f79815
Bianca Popescu <inktense12@gmail.com>
2022-07-02 12:53:01 +0100

    Small fix

--- sketches/june/concentricCircles.js
+++ sketches/june/concentricCircles.js
@@ -1,32 +1,42 @@
 import { addMarginToCanvas } from "../../utils/canvasSettings";
 import { getColorPalette } from "../../utils/helpers";
 import { sample } from "lodash";
+import { random } from "canvas-sketch-util";
 
-export const dimensions = "A4";
-export const baseColor = "#212d40";
+export const dimensions = [1080, 1080];
+export const baseColor = "#264653";
 
 export const generateSketch = (context, width, height) => {
   // Retrieveing actual working space
   const { gridW, gridH, margX, margY } = addMarginToCanvas(width, height);
-  const cols = 15;
-  const rows = 15;
-  const cellW = gridW / cols;
-  const cellH = gridH / rows;
-  
+  const cols = 5;
+  const rows = 5;
   const numCells = cols * rows;
 
   const colorsPallete = getColorPalette();
 
   for (let i = 0; i < numCells; i++) {
-    // i % 4 = 0, 1, 2, 3...
-    const col = i % cols;
-    // Math.floor(i / 4) = 0, 0, 0, 0, 1, 1, 1, 1...
-    const row = Math.floor(i / cols);
-    // Width of a collumn cell
-    const offsetW = cellW * col;
-    // Height of a collumn cell
-    const offsetH = cellH * row;
 
+    const x = random.range(margX, gridW);
+    const y = random.range(margY, gridH);
+    const radius = random.range(5, 25);
 
+    context.beginPath();
+    context.arc(x, y, radius, 0, 2 * Math.PI);
+    context.fillStyle = sample(colorsPallete);
+    context.fill();
+
+    for (let j = 0; j < random.range(0, 10); j++) {
+      context.beginPath();
+      context.arc(x, y, radius + j * random.range(10, 40), 0, 2 * Math.PI);
+
+      if (random.boolean()) {
+        context.fillStyle = sample(colorsPallete);
+        context.fill();
+      } else {
+        context.strokeStyle = sample(colorsPallete);
+        context.stroke();
+      }
+    }
   }
 };
